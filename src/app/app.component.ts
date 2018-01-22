e39875a16fd6fff79f263510a6b3ca34d2df3f79
Joshua Waggoner <joshua@turncommerce.com>
2018-01-22 12:04:39 -0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular Internationalization (i18n) Example';
+  now = new Date();
+
+  languages = [
+    { locale: 'en', display: 'English', link: 'http://localhost:5280/en/' },
+    { locale: 'fr', display: 'Français', link: 'http://localhost:5281/fr/' },
+    { locale: 'ch', display: 'Chinese (Traditional)', link: 'http://localhost:5282/ch/' }
+  ];
+
+  imgTitle = 'Angular Logo';
+  /*tslint:disable*/
+  logo = 'https://pbs.twimg.com/media/DSZwnjSW0AA8vU_.jpg';
+  /*tslint:enable*/
+
 }
